c00d8ac9815a289a94c301483466690d8c7e489a
Ray Heinonen <Ray_heinonen@hotmail.com>
2023-09-24 17:14:58 -0700

    update form button

--- app/join/page.jsx
+++ app/join/page.jsx
@@ -9,18 +9,14 @@ const Join = () => {
         </h1>
         <p className="text-lg md:text-xl text-gray-700 mb-4 md:mb-6">
           Click the link and fill out the form!
-        </p>
-        <p className="text-lg md:text-xl text-blue-500 mb-4 md:mb-6 font-bold">
-          Form will open up closer to Fall Quarter! Check back soon!
-        </p>
-        {/* <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSfzmitDLmZdLhk4OnQ1oozAyLVHGvpxK9OOtd1UlXhuf38X1w/viewform?usp=sf_link"
+        </p><a
+          href="https://linktr.ee/tritonpickleball"
           target="_blank"
           rel="noopener noreferrer"
           className="block w-44 mx-auto py-2 md:py-3 bg-blue-500 text-white rounded-full text-center hover:bg-blue-600"
         >
           Form
-        </a> */}
+        </a>
       </div>
       <div className="mt-8 md:mt-12 max-w-3xl p-6 md:p-10 rounded-xl shadow-xl">
         <h1 className="text-3xl md:text-4xl font-semibold mb-4 md:mb-6">
